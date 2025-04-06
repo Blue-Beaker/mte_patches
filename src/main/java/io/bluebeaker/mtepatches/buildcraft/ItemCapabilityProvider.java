f65f857b3dcd55925c6892ecf81b08c2fa810d80
blue-beaker <yyy5566778899@126.com>
2025-04-06 13:09:37 +0800

    Clean code

--- src/main/java/io/bluebeaker/mtepatches/buildcraft/ItemCapabilityProvider.java
+++ src/main/java/io/bluebeaker/mtepatches/buildcraft/ItemCapabilityProvider.java
@@ -21,16 +21,27 @@ public class ItemCapabilityProvider implements ICapabilityProvider {
 
     @Override
     public boolean hasCapability(@Nonnull Capability<?> capability, @Nullable EnumFacing facing) {
-        if(MTEPatchesConfig.buildcraft.itemPipeAcceptEjection && capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY){
+        if(shouldAddCapability(capability,facing)) {
             return tile.hasCapability(CapUtil.CAP_ITEM_TRANSACTOR, facing);
         }
         return false;
     }
 
+    protected boolean shouldAddCapability(@Nonnull Capability<?> capability, @Nullable EnumFacing facing){
+        if(!(MTEPatchesConfig.buildcraft.itemPipeAcceptEjection && (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY))) return false;
+        if(facing==null) return false;
+//        TileEntity neighbourTile = this.tile.getNeighbourTile(facing);
+
+//        MTEPatchesMod.getLogger().info("Getting capability of {} from side {} : {}",tile.getPos(),facing,tile.getNeighbourTile(facing));
+
+//        if(neighbourTile instanceof IPipeHolder) return false;
+        return true;
+    }
+
     @Nullable
     @Override
     public <T> T getCapability(@Nonnull Capability<T> capability, @Nullable EnumFacing facing) {
-        if(MTEPatchesConfig.buildcraft.itemPipeAcceptEjection && capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY){
+        if(shouldAddCapability(capability,facing)){
             IItemTransactor transactor = tile.getCapability(CapUtil.CAP_ITEM_TRANSACTOR, facing);
             if(transactor!=null)
                 return (T) new ItemPipeHandler(transactor);
