0a964174522708994646394fb75a8e40181aaa5c
blue-beaker <yyy5566778899@126.com>
2025-04-06 15:23:32 +0800

    mj ratio

--- src/main/java/io/bluebeaker/mtepatches/buildcraft/BCUtils.java
+++ src/main/java/io/bluebeaker/mtepatches/buildcraft/BCUtils.java
@@ -1,12 +1,13 @@
 package io.bluebeaker.mtepatches.buildcraft;
 
+import buildcraft.api.mj.MjAPI;
 import io.bluebeaker.mtepatches.MTEPatchesConfig;
 
 public class BCUtils {
     public static int convertMJtoFE(long mj){
-        return (int) (mj*MTEPatchesConfig.buildcraft.mjToForgeEnergyRatio);
+        return (int) ((double) mj /MjAPI.MJ*MTEPatchesConfig.buildcraft.mjToForgeEnergyRatio);
     }
     public static long convertFEtoMJ(int fe){
-        return (long) (fe/MTEPatchesConfig.buildcraft.mjToForgeEnergyRatio);
+        return (long) (fe*MjAPI.MJ/MTEPatchesConfig.buildcraft.mjToForgeEnergyRatio);
     }
 }
