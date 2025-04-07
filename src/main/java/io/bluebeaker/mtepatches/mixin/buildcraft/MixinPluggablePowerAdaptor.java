9a8e8a9d9f84bf00b15fa35b128c3d7c2fb792ba
blue-beaker <yyy5566778899@126.com>
2025-04-07 15:53:33 +0800

    Fix power adaptor not working on receiving FE

---
+++ src/main/java/io/bluebeaker/mtepatches/mixin/buildcraft/MixinPluggablePowerAdaptor.java
@@ -0,0 +1,35 @@
+package io.bluebeaker.mtepatches.mixin.buildcraft;
+
+import buildcraft.api.mj.IMjConnector;
+import buildcraft.api.mj.MjAPI;
+import buildcraft.api.transport.pipe.IPipeHolder;
+import buildcraft.api.transport.pluggable.PipePluggable;
+import buildcraft.api.transport.pluggable.PluggableDefinition;
+import buildcraft.transport.plug.PluggablePowerAdaptor;
+import io.bluebeaker.mtepatches.MTEPatchesConfig;
+import io.bluebeaker.mtepatches.buildcraft.EnergyAdaptorMJtoFE;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.energy.CapabilityEnergy;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = PluggablePowerAdaptor.class,remap = false)
+public abstract class MixinPluggablePowerAdaptor extends PipePluggable {
+
+    public MixinPluggablePowerAdaptor(PluggableDefinition definition, IPipeHolder holder, EnumFacing side) {
+        super(definition, holder, side);
+    }
+
+    @Inject(method = "getCapability(Lnet/minecraftforge/common/capabilities/Capability;)Ljava/lang/Object;",at=@At("RETURN"),cancellable = true)
+    public <T> void getCapability(Capability<T> cap, CallbackInfoReturnable<T> cir){
+        if(MTEPatchesConfig.buildcraft.mjToForgeEnergy && cir.getReturnValue()==null && cap == CapabilityEnergy.ENERGY){
+            IMjConnector capability = (IMjConnector) holder.getPipe().getBehaviour().getCapability(MjAPI.CAP_CONNECTOR, side);
+            if(capability!=null){
+                cir.setReturnValue((T) new EnergyAdaptorMJtoFE(capability));
+            }
+        }
+    }
+}
