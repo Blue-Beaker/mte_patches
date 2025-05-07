28c903cfd6764f11e9db383759b4fc0126bf43d3
blue-beaker <yyy5566778899@126.com>
2025-05-07 18:12:31 +0800

    Fix IC2 LAN message fix

--- src/main/java/io/bluebeaker/mtepatches/mixin/ic2/MixinPlatformClient.java
+++ src/main/java/io/bluebeaker/mtepatches/mixin/ic2/MixinPlatformClient.java
@@ -1,8 +1,10 @@
 package io.bluebeaker.mtepatches.mixin.ic2;
 
+import ic2.core.Platform;
 import ic2.core.PlatformClient;
 import io.bluebeaker.mtepatches.MTEPatchesConfig;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.text.TextComponentTranslation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,17 +12,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = PlatformClient.class,remap = false)
-public abstract class MixinPlatformClient {
+public abstract class MixinPlatformClient extends Platform {
     @Inject(method = "messagePlayer(Lnet/minecraft/entity/player/EntityPlayer;Ljava/lang/String;[Ljava/lang/Object;)V",at = @At("HEAD"),cancellable = true)
     public void messagePlayer(EntityPlayer player, String message, Object[] args, CallbackInfo ci){
         if(!MTEPatchesConfig.ic2.lanMessageFix) return;
         TextComponentTranslation textComponentTranslation;
         if (args.length > 0) {
-            textComponentTranslation = new TextComponentTranslation(message, (Object[])((AccessorPlatform)this).getMessageComponents(args));
+            textComponentTranslation = new TextComponentTranslation(message, (Object[])(this.getMessageComponents(args)));
         } else {
             textComponentTranslation = new TextComponentTranslation(message);
         }
-        player.sendMessage(textComponentTranslation);
+        ((EntityPlayerMP)player).sendMessage(textComponentTranslation);
         ci.cancel();
     }
 }
